b78d78c1f80d3809c56f7c1b1ba964bdc9f55806
Fish Milnikiewicz <sam464@cornell.edu>
2017-02-25 15:03:26 -0500

    base for website

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="scripts/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="scripts/script.js"></script>
+	<title>Home</title>
+</head>
+<body onload="startGame()">
+	<h1>This is a header</h1>
+	<div id="buttons">
+  		<button onclick="moveup()">UP</button><br><br>
+  		<button onclick="moveleft()">LEFT</button>
+  		<button onclick="moveright()">RIGHT</button><br><br>
+  		<button onclick="movedown()">DOWN</button>
+	</div>
+</body>
+</html>
