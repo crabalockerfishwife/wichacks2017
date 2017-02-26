910484a9320b4485c489c22f83269f322076618d
Carina Cheng <chc94@cornell.edu>
2017-02-25 20:42:27 -0500

    lk

--- index.php
+++ index.php
@@ -4,20 +4,15 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="scripts/jquery-3.1.1.min.js"></script>
-	<script type="text/javascript" src="scripts/script2.js"></script>
+	<script type="text/javascript" src="scripts/script.js"></script>
 	<title>Home</title>
 </head>
 <body onload="startGame()">
 	<h1>This is a header</h1>
 	<div style="text-align:center;width:480px;">
-		<!--
  		<button onmousedown="moveup()" onmouseup="clearmove()" ontouchstart="moveup()">UP</button><br><br>
  		<button onmousedown="moveleft()" onmouseup="clearmove()" ontouchstart="moveleft()">LEFT</button>
   		<button onmousedown="moveright()" onmouseup="clearmove()" ontouchstart="moveright()">RIGHT</button><br><br>
-<<<<<<< HEAD
-  		<button onmousedown="movedown()" onmouseup="clearmove()" ontouchstart="movedown()">DOWN</button>
-		-->
-=======
   	<button onmousedown="movedown()" onmouseup="clearmove()" ontouchstart="movedown()">DOWN</button>
 	</div>
 </body>
